6f0c893e83b9fd9d2084554ed81195328686c235
Mateusz Wasik <mateusz.wasik@4finance.com>
2018-02-26 10:51:15 +0100

    .

--- src/main/java/com/springboot/springboot/UserController.java
+++ src/main/java/com/springboot/springboot/UserController.java
@@ -20,23 +20,23 @@ public class UserController {
     }
 
     @RequestMapping("/users/{id}")
-    public User getUser(@PathVariable("id") int id){
+    public User getUser(@PathVariable("id") int id) {
         return userService.getUser(id);
     }
 
     @RequestMapping(method = POST, value = "/users")
-    public void addUser(@RequestBody User user){
+    public void addUser(@RequestBody User user) {
         userService.addUser(user);
     }
 
     @RequestMapping(method = PUT, value = "/users/{id}")
-    public void updateUser(@RequestBody User user, @PathVariable("id") int id){
-        userService.updateUser(user,id);
+    public void updateUser(@RequestBody User user, @PathVariable("id") int id) {
+        userService.updateUser(user, id);
     }
 
-    @RequestMapping(method = DELETE, value = "/users")
-    public void deleteUser(@RequestBody User user){
-        userService.deleteUser(user);
+    @RequestMapping(method = DELETE, value = "/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
     }
 
 }
