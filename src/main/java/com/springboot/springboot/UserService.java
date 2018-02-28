83598eec4c16afacd2aab91c5c9e2f0f868ca5ea
Mateusz Wasik <mateusz.wasik@4finance.com>
2018-02-28 08:32:48 +0100

    Added angular frontend

--- src/main/java/com/springboot/springboot/UserService.java
+++ src/main/java/com/springboot/springboot/UserService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -12,12 +11,6 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    private List<User> users = new ArrayList<>(Arrays.asList(
-            new User(1, "Mateusz"),
-            new User(2, "Monika"),
-            new User(3, "Robert")
-    ));
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
@@ -27,15 +20,15 @@ public class UserService {
     }
 
     public User getUser(int id) {
-       return userRepository.findOne(id);
+        return userRepository.findOne(id);
     }
 
     public void addUser(User user) {
         userRepository.save(user);
     }
 
-    public void deleteUser(int id){
-    userRepository.delete(id);
+    public void deleteUser(int id) {
+        userRepository.delete(id);
     }
 
     public void updateUser(User user, int id) {
